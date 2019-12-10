1414d50af4751da4f3f3cad56b08e01681db2189
ncl7 <ncl7@njit.edu>
2019-12-10 18:53:41 -0500

    Feature: added code for config file in src

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# /src/config.py
+
+import os
+
+class Development(object):
+    """
+    Development environment configuration
+    """
+    DEBUG = True
+    TESTING = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')

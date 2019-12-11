91585e61e331482d11ba4a6145b0af59ae513eeb
ncl7 <ncl7@njit.edu>
2019-12-10 19:57:34 -0500

    Fix: updated app file for src

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ from flask import Flask
 # from .config import app_config
 from .models import db, bcrypt
 
+# import user_api blueprint
+from .views.UserView import user_api as user_blueprint # add this line
+
 
 def create_app(env_name):
     """

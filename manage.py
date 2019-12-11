82144e33a016060ab6dc7c0251f7333469a0d7c3
ncl7 <ncl7@njit.edu>
2019-12-10 19:44:17 -0500

    Feature: added code to manage file

---
+++ manage.py
@@ -0,0 +1,10 @@
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from src.app import create_app, db
+
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
+
+migrate = Migrate(app=app, db=db)

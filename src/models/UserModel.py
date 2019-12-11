defc8c70a6497f4c6bf1f35556e35cf8a3bc112f
ncl7 <ncl7@njit.edu>
2019-12-10 19:40:11 -0500

    Fix: updated UserModel file

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -1,13 +1,10 @@
 # src/models/UserModel.py
 from marshmallow import fields, Schema
 import datetime
-from ..app import bcrypt
+from .BlogpostModel import BlogpostSchema
 
 
 class UserModel(db.Model):
     """
     User Model
     """
-
-
-# table name

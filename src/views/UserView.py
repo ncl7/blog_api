92652c2db78565cdda652e45bfcb775a79571b2d
ncl7 <ncl7@njit.edu>
2019-12-10 19:50:12 -0500

    Feature: added code for UserView file

---
+++ src/views/UserView.py
@@ -0,0 +1,11 @@
+#/src/views/UserView
+
+from flask import request, json, Response, Blueprint
+# from ..models.UserModel import UserModel, UserSchema
+# from ..shared.Authentication import Auth
+
+user_api = Blueprint('users', __name__)
+# user_schema = UserSchema()
+
+# @user_api.route('/', methods=['POST'])
+# def create():

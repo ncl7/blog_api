b3dbadee87c99d820c785a1922537cab592fd253
ncl7 <ncl7@njit.edu>
2019-12-16 21:57:34 -0500

    Fix: corrected code for config

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@
 
 import os
 
+
 class Development(object):
     """
     Development environment configuration
@@ -9,3 +10,20 @@ class Development(object):
     DEBUG = True
     TESTING = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
+
+class Production(object):
+    """
+    Production environment configurations
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+
+
+app_config = {
+    'development': Development,
+    'production': Production,
+}

d943554f38d8f012ecc44ece9320046154656aed
ncl7 <ncl7@njit.edu>
2019-12-16 21:35:07 -0500

    Feature: added more functions for BlogpostView

--- src/views/BlogpostView.py
+++ src/views/BlogpostView.py
@@ -7,5 +7,30 @@ blogpost_api = Blueprint('blogpost_api', __name__)
 blogpost_schema = BlogpostSchema()
 
 
-# @blogpost_api.route('/', methods=['POST'])
-# @Auth.auth_required
+@blogpost_api.route('/', methods=['POST'])
+@Auth.auth_required
+def create():
+    """
+  Create Blogpost Function
+  """
+    # app initiliazation
+    #####################
+    # existing code remain #
+    ######################
+    return custom_response(data, 201)
+
+
+# add this function
+@blogpost_api.route('/', methods=['GET'])
+def get_all():
+    """
+  Get All Blogposts
+  """
+    posts = BlogpostModel.get_all_blogposts()
+    data = blogpost_schema.dump(posts, many=True).data
+    return custom_response(data, 200)
+
+# app initiliazation
+#####################
+# existing code remain #
+######################

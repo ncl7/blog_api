f221b1d2d338a5a5ffb26a4197b6eaca133115cd
ncl7 <ncl7@njit.edu>
2019-12-16 22:21:51 -0500

    Fix: corrected code for manage file

--- manage.py
+++ manage.py
@@ -8,3 +8,10 @@ env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
 
 migrate = Migrate(app=app, db=db)
+
+manager = Manager(app=app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+  manager.run()

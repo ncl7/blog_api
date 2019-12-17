1c32437cb02e4928ea7a76843973f1d74aca83d6
ncl7 <ncl7@njit.edu>
2019-12-16 22:18:01 -0500

    Feature: added code for owner ID

--- src/models/BlogpostModel.py
+++ src/models/BlogpostModel.py
@@ -1,6 +1,7 @@
 # src/models/BlogpostModel.py
 from . import db
 import datetime
+from marshmallow import fields, Schema
 
 
 class BlogpostModel(db.Model):
@@ -13,12 +14,14 @@ class BlogpostModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128), nullable=False)
     contents = db.Column(db.Text, nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_at = db.Column(db.DateTime)
     modified_at = db.Column(db.DateTime)
 
     def __init__(self, data):
         self.title = data.get('title')
         self.contents = data.get('contents')
+        self.owner_id = data.get('owner_id')
         self.created_at = datetime.datetime.utcnow()
         self.modified_at = datetime.datetime.utcnow()
 
@@ -34,7 +37,7 @@ class BlogpostModel(db.Model):
 
     def delete(self):
         db.session.delete(self)
-        de.session.commit()
+        db.session.commit()
 
     @staticmethod
     def get_all_blogposts():
